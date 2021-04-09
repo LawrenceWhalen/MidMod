bcb0b5a2418b5a6f00a4ac5b7590400846553569
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-09 08:02:24 -0600

    Red enroll method for course

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -21,10 +21,29 @@ RSpec.describe "Course" do
     end
   end
   describe "#full" do
-    it "checks if the number of students matches the course capacity" do
+    it "can tell when a course isn't full" do
       course = Course.new("Calculus", 2)
 
       expect(course.full?).to eq(false)
+    end
+    it "can tell when a course is full" do
+      course = Course.new("Calculus", 2)
+
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+    end
+  end
+  describe "#enroll" do
+    it "adds a student instance to the students state" do
+      course = Course.new("Calculus", 2)
+
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+
+      course.enroll(student1)
+      course.enroll(student2)
+
+      exect(course.students).to eq([student1, student2])
     end
   end
 end
