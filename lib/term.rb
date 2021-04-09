c51997fe778c8429d88a2cf18ab0a08a110f3457
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-09 09:16:44 -0600

    Red green initialize and basic methods for term

--- lib/term.rb
+++ lib/term.rb
@@ -2,7 +2,15 @@ require "./lib/student"
 require "./lib/course"
 
 class Term
+  attr_reader :name,
+              :courses
 
   def initialize(name)
+    @name = name
+    @courses = []
+  end
+
+  def add_course(course)
+    @courses.push(course)
   end
 end
